0fb7f35c77a265f827a519f5e64aae51f7a05445
Ester Hugosson <eh225bx@student.lnu.se>
2024-11-17 20:15:07 +0000

    start of formular

--- src/js/components/recepie-formular/recepie-formular.js
+++ src/js/components/recepie-formular/recepie-formular.js
@@ -5,19 +5,42 @@
  * @version 1.1.0
  */
 
+import { StorageWrapper } from "../storageWrapper/storageWrapper.js"
 // Define a template
 const template = document.createElement('template')
 template.innerHTML = `
 
+    <style>
+    {
+    margin-bottom: 20px;
+    }
+
+    input {
+    display: block;
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 100%;
+    }
+
+    button {
+    padding: 10px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    cursor: pointer;
+    }
+    </style>
+
+    <h2> Add a recipe:</h2>
+
     <form id="recipeForm">
-        <input type="text" id="recipeTitle" placeholder="Recipe Title" required>
-        <input type="url" id="recipeLink" placeholder="Recipe URL" required>
-        <textarea id="recipeNotes" placeholder="Notes (optional)"></textarea>
+        <input type="text" id="recipeTitle" placeholder="Recipe Title" >
+        <input type="url" id="recipeLink" placeholder="Recipe URL" >
         <button type="submit">Add Recipe</button>
     </form>
 
 `
-customElements.define('recepie-formular', 
+customElements.define('recepie-formular',
 
     class extends HTMLElement {
 
@@ -25,8 +48,47 @@ customElements.define('recepie-formular',
             super()
 
             this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true))
+
+            this.form = this.shadowRoot.querySelector('#recipeForm')
+            this.titleInput = this.shadowRoot.querySelector('#recipeTitle')
+            this.linkInput = this.shadowRoot.querySelector('#recipeLink')
+            this.noteInput = this.shadowRoot.querySelector('#recipeNotes')
+
+            this.wrapper = new StorageWrapper()
         }
 
+        connectedCallback() {
+            this.form.addEventListener('submit', (event) => this.handleRecepieSubmit(event))
+        }
+
+        disconnectedCallback() {
+            this.form.removeEventListener('submit', this.handleRecepieSubmit)
+        }
+
+        handleRecepieSubmit(event) {
+            event.preventDefault()
+
+            const title = this.titleInput.value
+            const link = this.linkInput.value
+
+            this.saveRecepieInStorage(title, link)
+
+            this.form.reset()
+            
+        }
+
+        saveRecepieInStorage(title, value) {
+
+            try {
+                this.wrapper.storeData(title, value)
+
+            } catch (error) {
+                console.error('Failed to save recipe:', error)
+                alert('Error: Could not save the recipe.')
+            }
+            
+            
+        }
 
 
 
