44235ed70972e23f6d2278d0b9031efb95ec4340
Ester Hugosson <eh225bx@student.lnu.se>
2024-11-17 20:15:18 +0000

    style

--- src/js/components/saved-recepies/saved-recepies.js
+++ src/js/components/saved-recepies/saved-recepies.js
@@ -9,6 +9,18 @@
 const template = document.createElement('template')
 template.innerHTML = `
 
+<style>
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 10px;
+} </style>
+
 <div class="saved-recepies">
 
     <h2>Saved Recipes</h2>
